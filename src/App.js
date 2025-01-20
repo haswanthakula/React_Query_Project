917caa07ea6ba0827fd7c3746f4c9c7006310a4b
Haswanth Akula <haswanthakula5@gmail.com>
2025-01-20 14:05:21 +0530

    Update routing for GitHub Pages deployment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
@@ -19,6 +19,7 @@ function App() {
           <Route path="/tanstack" element={<Tanstack />} />
           <Route path="/traditional" element={<Traditional />} />
           <Route path="/mutation" element={<Mutation />} />
+          <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </Router>
     </QueryClientProvider>
